aa0f1eb54a6d2bb649ae8fd5f7411fd30ed19886
Joan Vila <48823226+vila-1748504@users.noreply.github.com>
2020-05-15 19:51:51 +0200

    aggiunto component utenze

--- pozzo.js
+++ pozzo.js
@@ -7,6 +7,34 @@ async function saveStatus() {
     return data
 }
 
+Vue.component('utenze', {
+    template: `
+    <div>
+        <div class="btn-group"><button v-for="sezione in sezioni" @click="setSezioneCorrente(sezione)" class="btn btn-primary">{{sezione}}</button></div>
+        <utenza v-for="utenza in utenzeAttive" v-bind:utenza="utenza" v-bind:key="utenza.id"></utenza>
+    </div>`,
+    props: ['utenze'],
+    data: function() {
+        return {
+            sezioneCorrente: '',
+            sezioni: { pozzo: 'pozzo', ossigenazione: 'ossigenazione', lavaggio: 'lavaggio', pescante: 'pescante' }
+        }
+    },
+    computed: {
+        utenzeAttive: function() {
+            var alias = this
+            return this.utenze.filter(function(utenza) { return utenza.sezione == alias.sezioneCorrente }) }
+    },
+    methods: {
+        setSezioneCorrente: function(sezione) {
+            this.sezioneCorrente = sezione
+        }
+    },
+    created: function() {
+        this.sezioneCorrente = this.sezioni.pozzo
+    }
+})
+
 Vue.component('utenza', {
     props: ['utenza'],
     template: `
@@ -16,7 +44,6 @@ Vue.component('utenza', {
             <button v-if="utenza.on" class="btn btn-danger" v-on:click="updateStatus(utenza)">SPEGNI</button>
             <button v-else class="btn btn-success" v-on:click="updateStatus(utenza)">ACCENDI</button><br>
         </div>`,
-    
     methods: {
         updateStatus: async function(utenza) {
             utenza.on = utenza.on ? false : true
@@ -28,23 +55,11 @@ Vue.component('utenza', {
 var app = new Vue({
     el: '#app',
     data: {
-        sezioneCorrente: "pozzo",
         utenze: []
     },
     created: async function() {
         const response = await fetch('/status.json')
-        var data = await response.json()
+        const data = await response.json()
         this.utenze = data
-    },
-    computed: {
-        utenzeAttive: function() {
-            var vm = this // importante! Non posso accedere all'istanza di Vue dall'interno della callback di .filter() quindi è necessario creare quest'alias
-            return this.utenze.filter(function(utenza) { return utenza.sezione == vm.sezioneCorrente })
-        }
-    },
-    methods: {
-        setSezioneCorrente: function(sezione) {
-            this.sezioneCorrente = sezione
-        }
     }
 })
