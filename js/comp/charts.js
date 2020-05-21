25c143fa3f0b771a3afbe0c8526da7842258bacc
Joan Vila <48823226+vila-1748504@users.noreply.github.com>
2020-05-21 18:23:56 +0200

    added water_cons & water_level basic charts

--- js/comp/charts.js
+++ js/comp/charts.js
@@ -1,77 +1,65 @@
 Vue.component('charts', {
+    template: `
+    <div class="row h-100px">
+        <div class="col">
+            <div class="card">
+            <div class="card-header">Water Consumption</div>
+            <chart :chart-data="wcd"></chart>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card">
+            <div class="card-header">Water Consumption</div>
+            <chart :chart-data="wcl"></chart>
+            </div>
+        </div>
+    </div>
+    `,
     props: {
         units: Array,
         water_cons: Array,
         water_level: Array
     },
+    computed: {
+        wcd: function() {
+            return {
+                labels: this.water_cons.map(e => e.date),
+                datasets: [{
+                    label: 'lt',
+                    backgroundColor: '#3232ff',
+                    data: this.water_cons.map(e => e.cons)
+                }]
+            }
+        },
+        wcl: function() {
+            return {
+                labels: this.water_level.map(e => e.date),
+                datasets: [{
+                    label: 'lt',
+                    backgroundColor: '#f87979',
+                    data: this.water_level.map(e => e.cons)
+                }]
+            }
+        }
+    }
+})
+
+Vue.component('chart',{
+    extends: VueChartJs.Bar,
+    mixins: [VueChartJs.mixins.reactiveProp],
     data: function() {
         return {
-            water_cons_id: 'water_cons',
         }
     },
-    computed: {
-        water_cons_labels: function() { return this.water_cons.map(function(entry) {return entry.date}) },
-        water_cons_data: function() { return this.water_cons.map(function(entry) {return entry.cons}) },
-        
-        water_level_labels: function() { return this.water_level.map(function(entry) {return entry.date}) },
-        water_level_data: function() { return this.water_level.map(function(entry) {return entry.cons}) }
-    },
-    template:
-    `
-    <div class="row">
-    <div class="col-sm-6">
-        <chart :id="water_cons_id" :labels="water_cons_labels" :data="water_cons_data"></chart>
-    </div>
-    <div class="col-sm-6">
-    </div>
-    </div>
-    `
+    mounted(){
+        this.renderChart(this.chartData, {legend: false, responsive: true})
+    }
 })
-
-Vue.component('chart', {
-    props: {
-        id: String,
-        labels: Array,
-        data: Array
-    },
-    // data: function() {
-    //     return {
-    //         el: {},
-    //         chart: {}
-    //     }
-    // },
-    created() {
-        var l = this.labels
-        var d = this.data
-        createChartOn(
-            this.id,
-            l,
-            d
-        )
-    },
-    template: `
-    <div class="card">
-        <div class="card-header">Water Consumption</div>
-        <div class="card-body">
-            <canvas :id="id" width="1300" height="520" style="display: block; height: 260px; width: 650px;" class="chartjs-render-monitor"></canvas>
-        </div>
-    </div>
-    `
-})
-
-// Bar Chart Example
-function createChartOn(id, l, d) {
-    return new Chart(document.getElementById(id), {
+/*  
+    element = document.getElementById(this.water_cons_id)
+    this.c = new Chart(element, {
         type: 'bar',
-        data: {
-            labels: l,
-            datasets: [{
-                label: "Revenue",
-                backgroundColor: "rgba(2,117,216,1)",
-                borderColor: "rgba(2,1,1,1)",
-                data: d,
-            }],
-        },
+        data: this.chartData,
         options: {
             scales: {
                 xAxes: [{
@@ -101,4 +89,4 @@ function createChartOn(id, l, d) {
             }
         }
     })
-}
+*/
