864c7301918f780c603c466690147022647b97f2
massimiliano pepparoni <massi910@Mmbp.local>
2020-06-03 00:40:45 +0200

    water graph last week support

--- src/js/comp/water-cons.vue
+++ src/js/comp/water-cons.vue
@@ -1,9 +1,20 @@
 <template>
-    <div>
-        <div class="card mt-3">
+    <div class="container-fluid border mt-4 p-2">
+        
+        <div class="card mb-2">
             <div class="card-header">Water Consumption</div>
             <barchart style="height: 80vh" :chart-data="wcd"></barchart>
         </div>
+
+        <div>
+            <button class="btn btn-outline-default btn-sm" v-on:click="changeData(() => true)">
+                Tutti
+            </button>
+            <button class="btn btn-outline-default btn-sm" v-on:click="changeData(lastSevenFilter)">
+                Ultimi sette giorni
+            </button>
+        </div>
+
     </div>
 </template>
 
@@ -11,6 +22,20 @@
 import barchart from './cmp-chart-bar'
 
 export default {
+    data: function() {
+        return {
+            /** Chart data */
+            wcd: {},
+            /** Date filter function */
+            lastSevenFilter: (date) => 
+            {
+                let today = new Date();
+                let diff = today.getTime() - new Date(date).getTime();
+                let diffDays = diff / (1000 * 3600 * 24);
+                return diffDays < 28;
+            }
+        }
+    },
     components:
     {
         barchart
@@ -19,16 +44,44 @@ export default {
         units: Array,
         water_cons: Array,
     },
-    computed: {
-        wcd: function() {
-            return {
+    watch:
+    {
+        water_cons:
+        {
+            /** each time water_cons changes the chart 
+             * data formatted object will be updated 
+            */
+            handler: function()
+            {
+                this.wcd = {
                 labels: this.water_cons.map(e => e.date),
                 datasets: [{
                     label: 'lt',
                     backgroundColor: '#3232ff',
                     data: this.water_cons.map(e => e.cons)
-                }]
-            }
+                }]}
+            },
+            deep: true,
+            immediate: true
+        }
+    },
+    methods:
+    {
+        /**
+         * Change the data according to the given
+         * filter function
+         * In order to change the chart rendering, the 
+         * entire object should be assign
+         */
+        changeData: function(filter)
+        {
+            this.wcd = {
+                labels: this.water_cons.map(e => e.date).filter(filter),
+                datasets: [{
+                    label: 'lt',
+                    backgroundColor: '#3232ff',
+                    data: this.water_cons.map(e => e.cons)
+                }]}
         }
     }
 }
--- src/js/comp/water-level.vue
+++ src/js/comp/water-level.vue
@@ -1,8 +1,17 @@
 <template>
-    <div>
-        <div class="card mt-3">
+    <div class="container-fluid border mt-4 p-2">
+        <div class="card mb-2">
             <div class="card-header">Water Level</div>
             <barchart style="height: 80vh" :chart-data="wld"></barchart>
+        </div>
+
+        <div>
+            <button class="btn btn-outline-default btn-sm" v-on:click="changeData(() => true)">
+                Tutti
+            </button>
+            <button class="btn btn-outline-default btn-sm" v-on:click="changeData(lastSevenFilter)">
+                Ultimi sette giorni
+            </button>
         </div>
     </div>
 </template>
@@ -15,14 +24,56 @@ export default {
     {
         barchart
     },
+    data () {
+      return {
+        wld: {},
+        /** Date filter function */
+        lastSevenFilter: (date) => 
+        {
+            let today = new Date();
+            let diff = today.getTime() - new Date(date).getTime();
+            let diffDays = diff / (1000 * 3600 * 24);
+            return diffDays < 28;
+        }
+      }
+    },
     props: {
         units: Array,
         water_level: Array,
     },
-    computed: {
-        wld: function() {
-            return {
+    watch:
+    {
+        water_level:
+        {
+            /** each time water_cons changes the chart 
+             * data formatted object will be updated 
+            */
+            handler: function()
+            {
+                this.wld = {
                 labels: this.water_level.map(e => e.date),
+                datasets: [{
+                    label: 'lt',
+                    backgroundColor: '#f87979',
+                    data: this.water_level.map(e => e.cons)
+                }]}
+            },
+            deep: true,
+            immediate: true
+        }
+    },
+    methods:
+    {
+        /**
+         * Change the data according to the given
+         * filter function
+         * In order to change the chart rendering, the 
+         * entire object should be assign
+         */
+        changeData: function(filter)
+        {
+            this.wld = {
+                labels: this.water_level.map(e => e.date).filter(filter),
                 datasets: [{
                     label: 'lt',
                     backgroundColor: '#f87979',
